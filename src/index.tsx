7d4be8f76f6a5bb839907cf6d286be1aa76a5aab
safin <safin.legend@gmail.com>
2022-11-29 09:29:48 +0200

    navbar

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,36 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import MyFirstComponent from './components/myFirstComponent';
+import SomeExampleComponent from './components/example/someExampleComponent';
+import './index.module.scss';
+import Navbar from './components/navbar/navbar';
+
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<App />}></Route>
+      <Route path='example' element={<SomeExampleComponent />} />
+      <Route path='/first' element={<MyFirstComponent />} />
+      <Route path='/navbar' element={<Navbar />}/>
+    </Routes>
+  </BrowserRouter>
+</React.StrictMode>
 );
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
