d1b0dce51213e6e4e817c002aa8ed909e511f490
oppilas oppilas <oppilas@ict.opetus.local>
2022-12-15 14:03:46 +0200

    ss

--- src/components/projects/project.tsx
+++ src/components/projects/project.tsx
@@ -9,20 +9,12 @@ type ProjectProps = {
 const Project: React.FC<ProjectProps> = () => (
   <div className={styles.container}>
     
-    <div className={styles.header}>
+    
+     <div className={styles.header}>
       <h1> My Projects</h1>
     </div>
 
-    <div className={styles.box}>
-      <img src={img} alt="" width={200}
-      height={200}
-      />
-
-      <h2 className={styles.header2}> The Lost Bunny Game</h2>
-
-      <h3 className={styles.header3}> This is a game I have created</h3>
-
-    </div>
+    
   </div>
 )
 
